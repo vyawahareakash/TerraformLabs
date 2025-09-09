302e10a6881ddc70d5a3708b431dbeadf8752501
vyawahareakash@gmail.com <vyawahareakash@gmail.com>
2025-09-09 10:01:44 +0530

    rg-update

---
+++ rg_module/rg.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "rgblock" {
+
+     name= var.rgname
+     location= var.rglocation
+
+}
---
+++ rg_module/var.tf
@@ -0,0 +1,12 @@
+variable "rgname" {
+
+    type = "string"
+    default = "rg-infra"
+}
+
+variable "rglocation" {
+
+
+    type = "string"
+    default = "Central India"
+}
